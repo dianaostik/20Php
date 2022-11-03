17fd5c81b7ccbfcaf645a2a2948d2c33163905d6
dianaostik <dianastud12@gmail.com>
2022-11-03 21:31:18 +0200

    3uzd

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
         $skaicius1 = rand(0,4);
         $skaicius2 = rand(0,4);
         echo("Pirmas skaicius: $skaicius1 <br> Antras skaicius: $skaicius2 <br>");
-        $tOne = ($skaicius1 > $skaicius2);
-        $tTwo = ($skaicius2 > $skaicius1);
+        $tOne = ($skaicius1 > $skaicius2 );
+        $tTwo = ($skaicius2 > $skaicius1 );
         if ($tOne) {
                     $rezOne = $skaicius1 / $skaicius2;
                     echo("Rezultatas = " . round($rezOne));
@@ -37,7 +37,23 @@
         echo "<br><br>";
 
         echo "------- 3 uzduotis ------- <br>";
-        
+        $num_aa = rand(0, 25);
+        $num_bb = rand(0, 25);
+        $num_cc = rand(0, 25);
+        echo("$num_aa , $num_bb , $num_cc <br>");
+
+        if ($num_aa===$num_bb || $num_bb===$num_cc || $num_cc===$num_aa) {
+            echo("Vienodos reiksmes");
+        } 
+        if ( ($num_aa < $num_bb && $num_bb < $num_cc) || ($num_aa > $num_bb && $num_bb > $num_cc)  ) {
+            echo("Reiksme =  $num_bb");
+        } 
+        if (($num_bb < $num_cc && $num_cc < $num_aa) || ($num_bb > $num_cc && $num_cc > $num_aa)) {
+            echo("Reiksme =  $num_cc");
+        } 
+        if(($num_cc < $num_aa && $num_aa < $num_bb) || ($num_cc > $num_aa && $num_aa > $num_bb)){
+            echo("Reiksme = $num_aa");
+        }
     ?>
 </body>
 </html>
